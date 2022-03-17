45fb021cf083f0390cfedab413cd74c4f4c40b87
Dylan Brandon <88333715+notTheRealOliverTwist@users.noreply.github.com>
2022-03-17 13:37:12 +0200

    Update index-promise.js

--- functions/sample/nodejs/index-promise.js
+++ functions/sample/nodejs/index-promise.js
@@ -1,28 +1,99 @@
 /**
  * Get all dealerships
- */
-
-const Cloudant = require('@cloudant/cloudant');
+ **/
 
 function main(params) {
 
-    const cloudant = Cloudant({
-        url: params.COUCH_URL,
-        plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
-    });
+        secret={
+            "COUCH_URL": "https://ba7b2ad7-040b-4eb0-a003-9d80c3c1ffd2-bluemix.cloudantnosqldb.appdomain.cloud",
+            "IAM_API_KEY": "XYo_lT0s-6mUE76LwaaAzqfmek2jOhCVkGtc8SjslTM5",
+            "COUCH_USERNAME": "ba7b2ad7-040b-4eb0-a003-9d80c3c1ffd2-bluemix"
+            };
 
-    let dbListPromise = getDbs(cloudant);
-    return dbListPromise;
-}
+        console.log(params);
 
-function getDbs(cloudant) {
-    return new Promise((resolve, reject) => {
-        cloudant.db.list()
-            .then(body => {
-                resolve({ dbs: body });
-            })
-            .catch(err => {
-                reject({ err: err });
+        return new Promise(function (resolve, reject) {
+
+            const Cloudant = require('@cloudant/cloudant');
+
+            const cloudant = Cloudant({
+                url: secret.COUCH_URL,
+                plugins: {iamauth: {iamApiKey:secret.IAM_API_KEY}}
             });
-    });
+
+            const dealershipDb = cloudant.use('dealerships');
+
+            if (params.state) {
+
+                // return dealership with this state
+
+                dealershipDb.find({
+
+                    "selector": {
+                        "state": {
+                            "$eq": params.state
+                            }
+                        }
+                    }, function (err, result) {
+
+                        if (err) {
+                            console.log("🚀 ~ file: index.js ~ line 20 ~ err", err)
+                            reject(err);
+                        }
+
+                        let code=200;
+
+                        if (result.docs.length==0)
+                        {
+                            code= 404;
+                        }
+                        resolve({
+                            statusCode: code,
+                            headers: { 'Content-Type': 'application/json' },
+                            body: result
+                            });
+                        });
+
+                    } else if (params.id) {
+
+                            id=parseInt(params.dealerId)
+
+                            // return dealership with this state
+                          dealershipDb.find({selector: {id:parseInt(params.id)}}, function (err, result) {
+                                if (err) {
+
+                                    console.log("🚀 ~ file: index.js ~ line 20 ~ err", err)
+                                    reject(err);
+                                }
+
+                                let code=200;
+
+                                if (result.docs.length==0)
+                                {
+                                    code= 404;
+                                }
+
+                                resolve({
+                                    statusCode: code,
+                                    headers: { 'Content-Type': 'application/json' },
+                                    body: result
+                                    });
+                                    });
+                            } else {
+                                    // return all documents
+
+                                    dealershipDb.list({ include_docs: true }, function (err, result) {
+                                        if (err) {
+                                            console.log("🚀 ~ file: index.js ~ line 35 ~ err", err)
+                                            reject(err);
+                                            }
+                                        resolve({
+                                            statusCode: 200,
+                                            headers: { 'Content-Type': 'application/json' },
+                                            body: result
+                                            });
+                                        });
+                                    }
+                            });
 }
+
