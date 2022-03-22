3b3e4cbac082ffc55a3d16c4426180aab0a26e1c
notTheRealOliverTwist <notreallyolivertwist@gmail.com>
2022-03-22 14:11:50 +0000

    initial commit

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
+from .restapis import get_dealers_from_cf
 import logging
 import json
 
@@ -94,15 +95,25 @@ def registration_request(request):
         else:
             return render(request, 'djangoapp/index.html', context)
 
-# Update the `get_dealerships` view to render the index page with a list of dealerships
+# Update the `get_dealerships` view to render the
+#  index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
-
+        url = "https://ba7b2ad7-040b-4eb0-a003-9d80c3c1ffd2-bluemix.cloudantnosqldb.appdomain.cloud"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+#def get_dealer_details(request, dealer_id):
+#    if request.method == "GET":
+#                url='https://ba7b2ad7-040b-4eb0-a003-9d80c3c1ffd2-bluemix.cloudantnosqldb.appdomain.cloud'
+#                dealerships = get_dealers_from_cf(url)
+                
+
+#                return HttpResponse(dealerships)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
