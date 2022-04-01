0e4887e8866ea2ad7cedc27e63df7dcf54e33431
Dylan Brandon <88333715+notTheRealOliverTwist@users.noreply.github.com>
2022-04-01 10:16:12 +0200

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,5 +9,11 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
     path(route='', view=views.get_dealerships, name='index'),
-    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details')
+    path(route='about', view=views.about, name='about'),
+    path(route='contact', view=views.contact, name='contact'),
+    path('registration/', view=views.registration_request, name='registration'),
+    path('login/', view=views.login_request, name='login'),
+    path('logout/', view=views.logout_request, name='logout'),
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+    path('review/<int:dealer_id>/', views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
