57e8ed6e47a52982eac6da7bf31b0d9c5da445cd
Dylan Brandon <88333715+notTheRealOliverTwist@users.noreply.github.com>
2022-04-11 19:04:35 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -51,7 +51,7 @@ class CarModel(models.Model):
 
     model = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     title = models.CharField(null=True, max_length=30, default='title')
-    dealerId = models.IntegerField(default=0)
+    id = models.IntegerField(default=0)
     carType = models.CharField(null=True, max_length=30, choices=Type_Choice, default=SEDAN)
     date = models.DateField(null=True)
     
