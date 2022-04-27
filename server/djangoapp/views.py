1f4d048f4b3f503a35852155a2d9c7f2b362ea37
Dylan Brandon <88333715+notTheRealOliverTwist@users.noreply.github.com>
2022-04-27 14:22:39 +0200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -88,7 +88,7 @@ def get_dealer_details(request, id):
         dealer = get_dealer_by_id_from_cf(dealer_url, id=id)
         context["dealer"] = dealer
     
-        review_url = "https://476b451e.eu-gb.apigw.appdomain.cloud/api/reviews"
+        review_url = "https://476b451e.eu-gb.apigw.appdomain.cloud/api/getreviews"
         reviews = get_dealer_reviews_from_cf(review_url, id=id)
         print(reviews)
         context["reviews"] = reviews
@@ -130,6 +130,6 @@ def add_review(request, id):
 
             new_payload = {}
             new_payload["review"] = payload
-            review_post_url = "https://476b451e.eu-gb.apigw.appdomain.cloud/api/reviews"
+            review_post_url = "https://476b451e.eu-gb.apigw.appdomain.cloud/api/postreviews"
             post_request(review_post_url, new_payload, id=id)
         return redirect("djangoapp:dealer_details", id=id)
