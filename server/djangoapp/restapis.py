3749184ab7bdfeedf18dbb186228f185dc5ffae9
Dylan Brandon <88333715+notTheRealOliverTwist@users.noreply.github.com>
2022-04-27 14:47:23 +0200

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,23 +1,26 @@
 import requests
 import json
-from .models import CarDealer, DealerReview, ReviewPost
+from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_watson.natural_language_understanding_v1 import Features,SentimentOptions
 import time
+ 
 
 def analyze_review_sentiments(text):
-    url = "https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/c8b0f019-31d6-41ac-b003-a2a31608839e"
-    api_key = "X2W_XG21E2BqmQ57cKeaX1rI9N43ZflG2KuaUmPJ_7wq"
+    url = "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/e9cf117d-4cf6-40df-ac58-7c43b602839f"
+    api_key = "dpPjzoKlkcqjL723aG5j5E8dLogKttjrnmSEi1B43zBA"
     authenticator = IAMAuthenticator(api_key)
     natural_language_understanding = NaturalLanguageUnderstandingV1(version='2021-08-01',authenticator=authenticator)
     natural_language_understanding.set_service_url(url)
     response = natural_language_understanding.analyze( text=text+"hello hello hello",features=Features(sentiment=SentimentOptions(targets=[text+"hello hello hello"]))).get_result()
     label=json.dumps(response, indent=2)
     label = response['sentiment']['document']['label']
-       
+    
+    
     return(label)
+
 
 def get_dealers_from_cf(url, **kwargs):
     results = []
@@ -26,7 +29,6 @@ def get_dealers_from_cf(url, **kwargs):
         json_result = get_request(url, state=state)
     else:
         json_result = get_request(url)
-    # print(json_result)    
 
     if json_result:
         # Get the row list in JSON as dealers
@@ -35,11 +37,10 @@ def get_dealers_from_cf(url, **kwargs):
         for dealer in dealers:
             # Get its content in `doc` object
             dealer_doc = dealer["doc"]
-            # print(dealer_doc)
             # Create a CarDealer object with values in `doc` object
-            dealer_obj = CarDealer(full_name=dealer_doc["full_name"], short_name=dealer_doc["short_name"], address=dealer_doc["address"], city=dealer_doc["city"],
-                                   id=dealer_doc["id"], st=dealer_doc["st"], lat=dealer_doc["lat"], long=dealer_doc["long"],
-                                   state=dealer_doc["state"], zip=dealer_doc["zip"])
+            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"],
+                                   full_name=dealer_doc["full_name"], id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
             results.append(dealer_obj)
 
     return results
@@ -52,9 +53,9 @@ def get_dealer_by_id_from_cf(url, id):
         
     
         dealer_doc = dealers["docs"][0]
-        dealer_obj = CarDealer(full_name=dealer_doc["full_name"], short_name=dealer_doc["short_name"], address=dealer_doc["address"], city=dealer_doc["city"],
-                                id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
-                                state=dealer_doc["state"],
+        dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"],
+                                full_name=dealer_doc["full_name"], id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                
                                 st=dealer_doc["st"], zip=dealer_doc["zip"])
     return dealer_obj
 
@@ -132,3 +133,4 @@ def post_request(url, payload, **kwargs):
     print("With status {} ".format(status_code))
     json_data = json.loads(response.text)
     return json_data
+
